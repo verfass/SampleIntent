b142524f45028574e55aeaaac3a3812d90d460c4
verfass <verfass@gmail.com>
2017-01-13 13:57:05 +0900

    result 받은 후 commit

--- app/src/main/java/kr/co/novacode/sampleintent/MainActivity.java
+++ app/src/main/java/kr/co/novacode/sampleintent/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == REQUEST_CODE_ANOTHER) {
+            Toast toast = Toast.makeText(getBaseContext(), "onActivityResult 메소드 호출됨 요청코드 : " + requestCode + ", 결과코드 : " + resultCode, Toast.LENGTH_LONG);
+            toast.show();
+
+            if (resultCode == RESULT_OK) {
+                String name = data.getExtras().getString("name");
+                toast = Toast.makeText(getBaseContext(), "응답으로 전환된 name : " + name, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
 }
